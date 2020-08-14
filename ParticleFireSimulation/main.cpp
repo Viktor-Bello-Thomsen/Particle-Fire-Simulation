b758bed46fe2fd862f8140bc1d13210db1bcce47
Haervaerk <45241565+Haervaerk@users.noreply.github.com>
2020-08-14 17:07:27 +0200

    Texture, Render and Buffer: Generel implementation, not in loop yet

--- ParticleFireSimulation/main.cpp
+++ ParticleFireSimulation/main.cpp
@@ -1,8 +1,85 @@
+#include<iostream>
 #include "SDL.h"
-
+using namespace std;
 int main(int argc, char* argv[]) {
 
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+	
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "SDL init failed" << endl;
+		return 1;
+	}
+	
+	SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", 
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+		SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
+	if (window == NULL) {
+		cout << SDL_GetError() << endl;
+		SDL_Quit();
+		return 2;
+	}
+
+	
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, 
+		SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	if (renderer == NULL) {
+		cout << "Could not create renderer" << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+	if (texture == NULL) {
+		cout << "Could not create texture" << endl;
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+	//maybe catch error for unsuccesful memory allocation
+
+	memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+	// 0xFF is just another way of writing 255, but in hexadecimal
+
+
+	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+	//Copying pixels to our buffer
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	//Passing our buffer to the renderer
+	SDL_RenderPresent(renderer);
+	//Presenting the renderer to window/screen
+
+	bool quit = false;
+	SDL_Event event;
+	while (!quit) {
+		//update particles
+		//draw particles
+		//check for message/events
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+	}
+	delete[] buffer;
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
+
+
+
+
+/*
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
@@ -13,5 +90,4 @@ int main(int argc, char* argv[]) {
 
 	SDL_Delay(3000);
 
-	return 0;
-}
+	*/
